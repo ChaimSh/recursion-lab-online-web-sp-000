050b690c47a170bcdf35a7d89332068e279fabeb
Kirill Chaim Shcherbina <rabbichaim770@gmail.com>
2020-08-07 00:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0])
+
+  if (string.length > 1) {
+    let mySubString = string.substring(1, string.length)
+    printString(mySubString)
+  } else {
+    return true
+  }
+}
+
+function reverseString(string) {
+  // console.log(string[string.length - 1])
+  if (string === "") {
+    return ""
+  } else {
+      return reverseString(string.substr(1)) + string[0]
+   }
+  }
+
+
+  function isPalindrome(myString) {
+    let l = myString.length;
+
+    if (l < 2) {
+      return true;
+    } else if (myString[l - 1] === myString[0]) {
+      return isPalindrome(myString.substring(1, l - 1));
+    } else {
+      return false;
+    }
+  }
+
+function addUpTo(myArray, index) {
+  return index ? myArray[index] + addUpTo(myArray, --index) : myArray[index];
+}
